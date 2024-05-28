36b361bb7ad5aee352b6cb179431298739767423
Kyle <kylejrgurlad@gmail.com>
2024-05-28 22:14:05 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # Convert the word to lowercase for case-insensitive comparison
+
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if sorted(word.lower()) == sorted(self.word) and word.lower() != self.word:
+                matches.append(word)
+        return matches
+
+
+     
